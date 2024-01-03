6e9e45318fa0abbc5bd891ad0bb94bacb75f8b97
Samson Ameh <amehsam69@gmail.com>
2024-01-03 11:57:43 +0100

    about page styled

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+import bgImg from "../assets/images/about-hero.png";
+
 export default function About() {
-  return <h1>About page</h1>;
+  return (
+    <section className="about-section">
+      <img src={bgImg} className="about-section-hero-image" />
+      <div className="about-section-content">
+        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉){" "}
+        </p>{" "}
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+      </div>
+      <div className="about-section-cta">
+        <h2>Your destination is waiting. Your van is ready.</h2>
+        <Link className="link-button" to="vans">
+          Explore our vans
+        </Link>
+      </div>
+    </section>
+  );
 }
