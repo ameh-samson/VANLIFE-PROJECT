5170cfd2f926d8d5e0489febed398a45f168d41f
Samson Ameh <amehsam69@gmail.com>
2024-01-04 17:22:03 +0100

    the van details was fetched
    using the id gotten from params, the vans detail was fetched and styled

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function VanDetail() {
-  return <h1>Van details goes here</h1>;
+  const params = useParams();
+  const [van, setVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
+  return (
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <p className="heading">{van.name}</p>
+          <p className="van-price">
+            ${van.price} <span className="duration">/day</span>
+          </p>
+          <p className="description">{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
