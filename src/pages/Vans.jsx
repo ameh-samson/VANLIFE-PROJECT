ac71345f0fc4b94eebdc6fe7eec1bed4db688810
Samson Ameh <amehsam69@gmail.com>
2024-01-04 13:17:14 +0100

    h1 added to the Van

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -24,6 +24,7 @@ export default function Vans() {
 
   return (
     <div className="van-list-section">
+      <h1>Explore our van options</h1>
       <div className="van-list-grid">{vanElements}</div>
     </div>
   );
