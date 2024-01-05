3c612fe873726c4e5686d1d7e1ccfbebea058a37
Samson Ameh <amehsam69@gmail.com>
2024-01-05 12:34:15 +0100

    function added to the activeLink

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,12 +1,33 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
+  const activeStyle = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
   return (
     <>
       <nav className="host-navbar">
-        <NavLink className={} to="/host">Dashboard</NavLink>
-        <NavLink to="/host/income">Income</NavLink>
-        <NavLink to="/host/reviews">Reviews</NavLink>
+        <NavLink
+          to="/host"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/host/income"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Income
+        </NavLink>
+        <NavLink
+          to="/host/reviews"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
