dedb1026d627ab00928084e08898f320e6225106
Samson Ameh <amehsam69@gmail.com>
2024-01-06 14:48:38 +0100

    fetch request made on hostvandetails

--- src/pages/host/HostVanDetail.jsx
+++ src/pages/host/HostVanDetail.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function HostVanDetail() {
-  return <h1>Host van details here</h1>;
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans[0]));
+  });
+
+  const currentVanSelected = (
+    <>
+      <img src={currentVan.imageUrl} width={150} alt={currentVan.name} />
+      <h2>{currentVan.name}</h2>
+      <p>{currentVan.price}</p>
+      <p>{currentVan.type}</p>
+    </>
+  );
+
+  return (
+    {currentVan?}
+  );
 }
