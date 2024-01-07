54fa87ddd55b39132dcf4ea62d5b570cfb4a8149
Samson Ameh <amehsam69@gmail.com>
2024-01-07 04:40:14 +0100

    the relative link ".." didn't fix it
    rather it goes back to the parent in the route

--- src/pages/host/HostVanDetail.jsx
+++ src/pages/host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function HostVanDetail() {
   const { id } = useParams();
@@ -16,18 +16,23 @@ export default function HostVanDetail() {
       {!currentVan ? (
         <h1>Loading.....</h1>
       ) : (
-        <div className="host-van-detail-layout-container">
-          <div className="host-van-detail">
-            <img src={currentVan.imageUrl} />
-            <div className="host-van-detail-info-text">
-              <i className={`van-type van-type-${currentVan.type}`}>
-                {currentVan.type}
-              </i>
-              <h3>{currentVan.name}</h3>
-              <h4>${currentVan.price}/day</h4>
+        <>
+          <Link to=".." className="back-button">
+            &larr; <span>Back to all vans</span>
+          </Link>
+          <div className="host-van-detail-layout-container">
+            <div className="host-van-detail">
+              <img src={currentVan.imageUrl} />
+              <div className="host-van-detail-info-text">
+                <i className={`van-type van-type-${currentVan.type}`}>
+                  {currentVan.type}
+                </i>
+                <h3>{currentVan.name}</h3>
+                <h4>${currentVan.price}/day</h4>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </section>
   );
