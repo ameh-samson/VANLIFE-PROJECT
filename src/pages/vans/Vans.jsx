3cb78d022e60ddcd6ac444d02f3058fec6b4f113
Samson Ameh <amehsam69@gmail.com>
2024-01-08 02:20:53 +0100

    search params hook added

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterType = searchParams.get("type");
+
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
