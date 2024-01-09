1b0e02aefc4ce4c37e5f9bc9b9b06240467f3371
Samson Ameh <amehsam69@gmail.com>
2024-01-09 14:33:23 +0100

    the fetch moved to seperate file and turned to async

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
+import { getVans } from "../../api";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
@@ -8,9 +9,12 @@ export default function Vans() {
   const filteredType = searchParams.get("type");
 
   useEffect(() => {
-    fetch("/api/vans")
-      .then((res) => res.json())
-      .then((data) => setVans(data.vans));
+    async function loadVans() {
+      const data = await getVans();
+      setVans(data);
+    }
+
+    loadVans();
   }, []);
 
   // filters the van based on URL query
